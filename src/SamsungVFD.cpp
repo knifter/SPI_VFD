4e0661ed1b7c1c51e34e891bda1b55d4ef66dc77
Tijs van Roon <knifter@gmail.com>
2019-05-26 14:12:02 +0200

    Rename lib, SW CS support, SPI Transaction support

--- src/SamsungVFD.cpp
+++ src/SamsungVFD.cpp
@@ -1,4 +1,4 @@
-#include "SPI_VFD.h"
+#include "SamsungVFD.h"
 
 #include <Arduino.h>
 
@@ -24,7 +24,7 @@
 //
 // Note, however, that resetting the Arduino doesn't reset the LCD, so we
 // can't assume that its in that state when a sketch starts (and the
-// SPI_VFD constructor is called).
+// constructor is called).
 
 // commands
 #define CMD_CLEARDISPLAY        0x01
@@ -65,168 +65,204 @@
 #define VFD_BRIGHTNESS100       0x00
 
 // SPI commmand/data select, will be or-ed with value
-#define VFD_SPICOMMAND          0xF800
-#define VFD_SPIDATA             0xFA00
+#define VFD_SPIDATA            		0xFA   	// RW=0, RS=1
+#define VFD_SPICOMMAND				0xF8	// RW=0, RS=1
+#define VFD_SPICOMMAND16        	(SPICOMMAND << 8) 
+#define VFD_SPIDATA16           	(SPIDATA << 8)
 
-SPI_VFD::SPI_VFD(SPIClass &spi, uint8_t cols, uint8_t lines)
- : _spi(spi), _numlines(lines)
+void SamsungVFD::begin()
 {
-}
-
-void SPI_VFD::begin() 
-{
-    _spi.begin();
-
-    // set number of lines
-    if (_numlines > 1)
-        _displayfunction = VFD_2LINE;
-    else
-        _displayfunction = VFD_1LINE;
-
-    // set the brightness and push the linecount with VFD_SETFUNCTION
-    setBrightness(VFD_BRIGHTNESS100);
+    // set the brightness(=0) and push the linecount with VFD_SETFUNCTION
+	_displayfunction = VFD_2LINE | VFD_BRIGHTNESS100;
+	command(CMD_FUNCTIONSET | _displayfunction);
 
     // Initialize to default text direction (for roman languages)
     _displaymode = VFD_ENTRYLEFT | VFD_ENTRYSHIFTDECREMENT;
-
-    // set the entry mode
     command(CMD_ENTRYMODESET | _displaymode);
-
-    command(CMD_SETDDRAMADDR);  // go to address 0
 
     // turn the display on with no cursor or blinking default
     _displaycontrol = VFD_DISPLAYON;
-    displayOn(true);
+    command(CMD_DISPLAYCONTROL | _displaycontrol);
 
-    clear();
-    home();
+	// go to address 0
+    command(CMD_SETDDRAMADDR); 
 }
 
 /********** high level commands, for the user! */
-void SPI_VFD::setBrightness(uint8_t brightness){
-  // set the brightness (only if a valid value is passed
-  if (brightness <= VFD_BRIGHTNESS25) {
-    _displayfunction &= ~VFD_BRIGHTNESS25;
-    _displayfunction |= brightness;
+void SamsungVFD::setBrightness(uint8_t brightness)
+{
+    // set the brightness (only if a valid value is passed
+    if (brightness <= VFD_BRIGHTNESS25)
+    {
+        _displayfunction &= ~VFD_BRIGHTNESS25;
+        _displayfunction |= brightness;
 
-	command(CMD_FUNCTIONSET | _displayfunction);
-  }
+        command(CMD_FUNCTIONSET | _displayfunction);
+    }
 }
 
-uint8_t SPI_VFD::getBrightness()
+uint8_t SamsungVFD::getBrightness()
 {
-  // get the brightness
-  return _displayfunction & VFD_BRIGHTNESS25;
+    // get the brightness
+    return _displayfunction & VFD_BRIGHTNESS25;
 }
 
-void SPI_VFD::clear()
+void SamsungVFD::clear()
 {
-  command(CMD_CLEARDISPLAY);  // clear display, set cursor position to zero
-  delayMicroseconds(2000);  // this command takes a long time!
+    command(CMD_CLEARDISPLAY); // clear display, set cursor position to zero
+    delayMicroseconds(2000);   // this command takes a long time!
 }
 
-void SPI_VFD::home()
+void SamsungVFD::home()
 {
-  command(CMD_RETURNHOME);  // set cursor position to zero
-  delayMicroseconds(2000);  // this command takes a long time!
+    command(CMD_RETURNHOME); // set cursor position to zero
+    delayMicroseconds(2000); // this command takes a long time!
 }
 
-void SPI_VFD::setCursor(uint8_t col, uint8_t row)
+void SamsungVFD::setCursor(uint8_t col, uint8_t row)
 {
-    int row_offsets[] = { 0x00, 0x40, 0x14, 0x54 };
-    if(row > 4)
-      return; 
+    int row_offsets[] = {0x00, 0x40, 0x14, 0x54};
+    if (row > 3)
+        return;
     command(CMD_SETDDRAMADDR | (col + row_offsets[row]));
 }
 
 // Turn the display on/off (quickly)
-void SPI_VFD::displayOn(bool on) 
+void SamsungVFD::displayOn(bool on)
 {
-  if(on)
-    _displaycontrol |= VFD_DISPLAYON;
-  else
-    _displaycontrol &= ~VFD_DISPLAYON;
-  command(CMD_DISPLAYCONTROL | _displaycontrol);
+    if (on)
+        _displaycontrol |= VFD_DISPLAYON;
+    else
+        _displaycontrol &= ~VFD_DISPLAYON;
+    command(CMD_DISPLAYCONTROL | _displaycontrol);
 }
 
 // Turns the underline cursor on/off
-void SPI_VFD::cursor(bool on) 
+void SamsungVFD::cursor(bool on)
 {
-  if(on)
-    _displaycontrol |= VFD_CURSORON;
-  else
-    _displaycontrol &= ~VFD_DISPLAYON;
-  command(CMD_DISPLAYCONTROL | _displaycontrol);
+    if (on)
+        _displaycontrol |= VFD_CURSORON;
+    else
+        _displaycontrol &= ~VFD_DISPLAYON;
+    command(CMD_DISPLAYCONTROL | _displaycontrol);
 }
 
 // Turn on and off the blinking cursor
-void SPI_VFD::blink(bool on) 
+void SamsungVFD::blink(bool on)
 {
-  if(on)
-    _displaycontrol |= VFD_BLINKON;
-  else
-    _displaycontrol &= ~VFD_BLINKON;
-  command(CMD_DISPLAYCONTROL | _displaycontrol);
+    if (on)
+        _displaycontrol |= VFD_BLINKON;
+    else
+        _displaycontrol &= ~VFD_BLINKON;
+    command(CMD_DISPLAYCONTROL | _displaycontrol);
 }
 
 // These commands scroll the display without changing the RAM
-void SPI_VFD::scrollDisplayLeft(void) 
+void SamsungVFD::scrollDisplayLeft(void)
 {
-  command(CMD_CURSORSHIFT | VFD_DISPLAYMOVE | VFD_MOVELEFT);
+    command(CMD_CURSORSHIFT | VFD_DISPLAYMOVE | VFD_MOVELEFT);
 }
-void SPI_VFD::scrollDisplayRight(void) 
+void SamsungVFD::scrollDisplayRight(void)
 {
-  command(CMD_CURSORSHIFT | VFD_DISPLAYMOVE | VFD_MOVERIGHT);
+    command(CMD_CURSORSHIFT | VFD_DISPLAYMOVE | VFD_MOVERIGHT);
 }
 
 // This is for text that flows Left to Right
-void SPI_VFD::leftToRight(void) 
+void SamsungVFD::leftToRight(void)
 {
-  _displaymode |= VFD_ENTRYLEFT;
-  command(CMD_ENTRYMODESET | _displaymode);
+    _displaymode |= VFD_ENTRYLEFT;
+    command(CMD_ENTRYMODESET | _displaymode);
 }
 
 // This is for text that flows Right to Left
-void SPI_VFD::rightToLeft(void) 
+void SamsungVFD::rightToLeft(void)
 {
-  _displaymode &= ~VFD_ENTRYLEFT;
-  command(CMD_ENTRYMODESET | _displaymode);
+    _displaymode &= ~VFD_ENTRYLEFT;
+    command(CMD_ENTRYMODESET | _displaymode);
 }
 
 // This will left/right 'justify' text from the cursor
-void SPI_VFD::autoscroll(bool on) 
+void SamsungVFD::autoscroll(bool on)
 {
-  if(on)
-    _displaycontrol |= VFD_ENTRYSHIFTINCREMENT;
-  else
-    _displaycontrol &= ~VFD_ENTRYSHIFTINCREMENT;
-  command(CMD_ENTRYMODESET | _displaymode);
+    if (on)
+        _displaycontrol |= VFD_ENTRYSHIFTINCREMENT;
+    else
+        _displaycontrol &= ~VFD_ENTRYSHIFTINCREMENT;
+    command(CMD_ENTRYMODESET | _displaymode);
 }
 
 // Allows us to fill the first 8 CGRAM locations
 // with custom characters
-void SPI_VFD::createChar(uint8_t location, uint8_t charmap[]) 
+void SamsungVFD::createChar(uint8_t location, uint8_t charmap[])
 {
-  location &= 0x7; // we only have 8 locations 0-7
-  command(CMD_SETCGRAMADDR | (location << 3));
-  for (int i=0; i<8; i++) 
-  {
-    write(charmap[i]);
-  }
+    location &= 0x7; // we only have 8 locations 0-7
+    command(CMD_SETCGRAMADDR | (location << 3));
+    for (int i = 0; i < 8; i++)
+    {
+        write(charmap[i]);
+    }
 }
 
 /*********** mid level commands, for sending data/cmds, init */
-inline void SPI_VFD::command(uint8_t value) 
+#ifdef SAMSUNGVFD_SPI_SWCS
+	#define	SAMSUNGVFD_CS_ON		   	if(_cs != 0xFF) digitalWrite(_cs, LOW);
+	#define	SAMSUNGVFD_CS_OFF		   	if(_cs != 0xFF) digitalWrite(_cs, HIGH);
+#else
+	#define SAMSUNGVFD_CS_ON
+	#define SAMSUNGVFD_CS_OFF
+#endif
+
+void SamsungVFD::command(uint8_t value)
 {
-    //SPI.beginTransaction(SPISettings(500000, MSBFIRST, SPI_MODE0));
-    _spi.transfer16(VFD_SPICOMMAND | value);
-    // SPI.endTransaction(); // release the SPI bus
+#ifdef SAMSUNGVFD_SPI_TRANSACTION
+    SPI.beginTransaction(_spisettings);
+#endif
+	SAMSUNGVFD_CS_ON;
+   	_spi.transfer16(SPICOMMAND16 | value);
+	SAMSUNGVFD_CS_OFF;
+#ifdef SAMSUNGVFD_SPI_TRANSACTION
+    SPI.endTransaction(); // release the SPI bus
+#endif
 };
 
-size_t SPI_VFD::write(uint8_t value) 
+size_t SamsungVFD::write(uint8_t value)
 {
-    // SPI.beginTransaction(SPISettings(500000, MSBFIRST, SPI_MODE0));
-    _spi.transfer16(VFD_SPIDATA | value);
-    // SPI.endTransaction(); // release the SPI bus
+#ifdef SAMSUNGVFD_SPI_TRANSACTION
+    SPI.beginTransaction(_spisettings);
+#endif
+	SAMSUNGVFD_CS_ON;
+    _spi.transfer16(SPIDATA16 | value);
+	SAMSUNGVFD_CS_OFF;
+
+#ifdef SAMSUNGVFD_SPI_TRANSACTION
+    SPI.endTransaction(); // release the SPI bus
+#endif
     return 1;
 }
+
+// Unsure if this loop would work on a HW CS platform like ESP32
+#ifdef SAMSUNGVFD_SPI_SWCS
+size_t SamsungVFD::write(const uint8_t *buffer, size_t size)
+{
+    uint8_t cnt = 0;
+#ifdef SAMSUNGVFD_SPI_TRANSACTION
+    SPI.beginTransaction(_spisettings);
+#endif
+
+	SAMSUNGVFD_CS_ON;
+	_spi.transfer(SPIDATA);
+	while (size--)
+	{
+		_spi.transfer(buffer[cnt++]);
+	};
+	SAMSUNGVFD_CS_OFF;
+
+#ifdef SAMSUNGVFD_SPI_TRANSACTION
+    SPI.endTransaction(); // release the SPI bus
+#endif
+    return cnt;
+}
+#endif // SAMSUNGVFD_SPI_SWCS
+
+#undef VFD_CHIPSELECT_ON
+#undef VFD_CHIPSELECT_OFF
--- src/SamsungVFD.h
+++ src/SamsungVFD.h
@@ -9,11 +9,20 @@
 // VFD Module 20T202DA2JA
 // Controller: µ PD16314
 
-class SPI_VFD : public Print 
+// Config, do not enable here but use a build define
+//#define SAMSUNGVFD_SPI_SWCS				// Enable Software CS control, with UseHwCs() its faster without
+//#define SAMSUNGVFD_SPI_TRANSACTION		// Enable using SPI.begin/endTransaction if multiple configs are used
+
+#define SAMSUNGVFD_CS_NONE				0xFF
+
+class SamsungVFD : public Print 
 {
     public:
-        SPI_VFD(SPIClass& spi, uint8_t cols = 20, uint8_t rows = 2);
-
+#ifdef SAMSUNGVFD_SPI_SWCS
+        SamsungVFD(SPIClass& spi, uint8_t cs = SAMSUNGVFD_CS_NONE) : _spi(spi), _cs(cs) {};
+#else
+        SamsungVFD(SPIClass& spi) : _spi(spi) {};
+#endif
         void begin();
 
         void clear();
@@ -31,21 +40,39 @@ class SPI_VFD : public Print
         void rightToLeft();
         void autoscroll(bool on = true);
 
-        void createChar(uint8_t, uint8_t[]);
-        void setCursor(uint8_t, uint8_t); 
-        virtual size_t write(uint8_t);
+        void createChar(uint8_t location, uint8_t charmap[]);
+        void setCursor(uint8_t col, uint8_t row);
 
-    private:
-        inline void command(uint8_t);
+        // Implement thsese for Print 
+        size_t write(uint8_t);
+#ifdef SAMSUNGVFD_SPI_SWCS
+		//Needs testing on HWCS platform first
+       size_t write(const uint8_t *buffer, size_t size);
+#endif
+    protected:
+        void command(uint8_t);
 
         SPIClass& _spi;
+		uint8_t _cs = -1;
+        SPISettings _spisettings = SPISettings(25000000, MSBFIRST, SPI_MODE3);
 
-        uint8_t _displayfunction;
-        uint8_t _displaycontrol;
-        uint8_t _displaymode;
+        uint8_t _displayfunction = 0x00;
+        uint8_t _displaycontrol = 0x00;
+        uint8_t _displaymode = 0x00;
 
-        uint8_t _numlines = 2;
-        uint8_t _currline = 0;
+        // uint8_t _numlines = 2;
 };
+
+// class SamsungVFD_SPI : public SamsungVFD
+// {
+//     public:
+//         SamsungVFD_SPI(SPIClass& spi, uint8_t cols = 20, uint8_t rows = 2) {};
+
+//         virtual size_t write(uint8_t);
+//         virtual size_t write(const uint8_t *buffer, size_t size);
+
+//     private:
+//         virtual void command(uint8_t);
+// };
 
 #endif
